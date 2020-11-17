8e7a1e8a74da199fcc9ccbf165b682b2273921da
Prince Odame <opodame@gmail.com>
2020-11-17 07:22:15 +0100

    Home component styling

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,28 @@
+import { Typography } from 'antd';
 import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
+import ControlPanel from '../features/controlPanel/ControlPanel';
 import styles from './Home.css';
+import googleFormIcon from '../images/google-forms-icon.png';
+
+const { Title } = Typography;
 
 export default function Home(): JSX.Element {
   return (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+    <div className={styles.container}>
+      <div className={styles.sider}>
+        <Title className={styles.title}>Auto Google Form Filling</Title>
+      </div>
+      <div className={styles.divider} />
+      <div className={styles.controlPanel}>
+        <ControlPanel />
+      </div>
+      <div className={styles.googleFormsIconContainer}>
+        <img
+          src={googleFormIcon}
+          alt="Google Forms Icon"
+          className={styles.googleFormIcon}
+        />
+      </div>
     </div>
   );
 }
