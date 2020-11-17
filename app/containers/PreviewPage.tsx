75b35a939e68f8c032c552eb3568e21c6e6433e2
Prince Odame <opodame@gmail.com>
2020-11-17 07:22:43 +0100

    Preview page

---
+++ app/containers/PreviewPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Preview from '../features/preview/Preview';
+import Page from './Page';
+
+export default function PreviewPage() {
+  return (
+    <Page>
+      <Preview />
+    </Page>
+  );
+}
